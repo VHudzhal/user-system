b84c0319343f64d90ccc8daba6f58a63a57decf1
nixage <xodanych98@mail.ru>
2020-02-14 15:04:56 +0200

    database setings

--- app.js
+++ app.js
@@ -81,15 +81,48 @@ const mysql = require("mysql");
 //   // database: "users",
 //   // password: "bealong123"
 // });
-const connection = mysql.createConnection('mysql://b5bec95840a84d:f8461cf6@us-cdbr-iron-east-04.cleardb.net/heroku_711fe32cc04199e?reconnect=true')
-connection.connect(function(err){
-  if (err) {
-      return console.error("Ошибка: " + err.message);
+// const connection = mysql.createConnection('mysql://b5bec95840a84d:f8461cf6@us-cdbr-iron-east-04.cleardb.net/heroku_711fe32cc04199e?reconnect=true')
+// connection.connect(function(err){
+//   if (err) {
+//       return console.error("Ошибка: " + err.message);
+//     }
+//     else{
+//       console.log("Подключение к серверу MySQL успешно установлено");
+//     }
+// });
+
+
+const db_config = {
+  host: 'us-cdbr-iron-east-04.cleardb.net',
+  user: 'b5bec95840a84d',
+  password: 'f8461cf6',
+  database: 'heroku_711fe32cc04199e'
+};
+
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
-});
+  });
+}
+
+handleDisconnect();
 /* ============================================================== */
 
 
